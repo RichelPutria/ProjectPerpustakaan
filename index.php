e8f3aaa8b8c049ceae7ce7f455f77edbaf860bf7
RichelPutria <88094629+RichelPutria@users.noreply.github.com>
2021-09-09 12:03:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="rag.jpg" alt="logo" style="width:40px;">
+  </a>
+  
+  <!-- Links -->
+
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php"> Home </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_siswa.php"> Data Siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php"> Data Kelas </a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container-fluid">
+  <h3> Selamat Datang di Perpustakaan CSF</h3>
+  <p>1. Menyediakan buku-buku yang menunjang kegiatan pembelajaran bagi pengguna jasa perpustakaan, baik untuk publik, guru/dosen maupun para siswa/mahasiswa.</p>
+  <p>2. Memberikan layanan yang berkaitan dengan informasi tertulis, digital, maupun bentuk media lainnya yang dibutuhkan oleh pengguna perpustakaan.</p>
+  <p>3. Memberikan layanan referensi yang membantu pengguna perpustakaan untuk mencari sumber informasi lainnya di luar perpustakaan yang dimaksud.
+</p>
+</div>
+
+</body>
+</html>
